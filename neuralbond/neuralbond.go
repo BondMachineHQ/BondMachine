6e36a84838811b9db0d1ed6c75c48977be830384
Mirko Mariotti <mirko@mirkomariotti.it>
2022-09-06 17:29:00 +0200

    templates filled

--- neuralbond/neuralbond.go
+++ neuralbond/neuralbond.go
@@ -86,9 +86,9 @@ func (n *TrainedNet) WriteBasm() (string, error) {
 			result += fmt.Sprintf("%%meta ioatt input_%d cp:node_0_%d, type:input, index:0\n", node.Pos, node.Pos)
 			result += fmt.Sprintf("%%meta ioatt input_%d cp:bm, type:input, index:%d\n", node.Pos, node.Pos)
 		} else if node.Type == "linear" {
-			result += fmt.Sprintf("%%meta cpdef node_%d_%d romcode:linear, inputs:%d\n", node.Layer, node.Pos, node.Inputs)
+			result += fmt.Sprintf("%%meta cpdef node_%d_%d romcode:linear, inputs:%d, bias:%f\n", node.Layer, node.Pos, node.Inputs, node.Bias)
 		} else if node.Type == "softmax" {
-			result += fmt.Sprintf("%%meta cpdef node_%d_%d romcode:softmax, inputs:%d\n", node.Layer, node.Pos, node.Inputs)
+			result += fmt.Sprintf("%%meta cpdef node_%d_%d romcode:softmax, inputs:%d, bias:%f\n", node.Layer, node.Pos, node.Inputs, node.Bias)
 		} else if node.Type == "output" {
 			result += fmt.Sprintf("%%meta cpdef node_%d_%d romcode:terminal\n", node.Layer, node.Pos)
 			result += fmt.Sprintf("%%meta iodef output_%d type:io\n", node.Pos)
@@ -103,8 +103,7 @@ func (n *TrainedNet) WriteBasm() (string, error) {
 		weightCP := fmt.Sprintf("weightcp_%d_%d__%d_%d", weight.Layer-1, weight.PosPrevLayer, weight.Layer, weight.PosCurrLayer)
 		downNode := fmt.Sprintf("node_%d_%d", weight.Layer-1, weight.PosPrevLayer)
 		upNode := fmt.Sprintf("node_%d_%d", weight.Layer, weight.PosCurrLayer)
-		// result += fmt.Sprintf("%%meta cpdef %s romcode:weight, weight:%f\n", weightCP, weight.Value)
-		result += fmt.Sprintf("%%meta cpdef %s romcode:weight\n", weightCP)
+		result += fmt.Sprintf("%%meta cpdef %s romcode:weight, weight:0f%f\n", weightCP, weight.Value)
 		result += fmt.Sprintf("%%meta iodef up%s type:io\n", weightCP)
 		result += fmt.Sprintf("%%meta iodef down%s type:io\n", weightCP)
 		result += fmt.Sprintf("%%meta ioatt down%s cp:%s, type:input, index:0\n", weightCP, weightCP)
