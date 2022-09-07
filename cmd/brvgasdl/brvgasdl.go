5cef80ee9eb255f6d8aa7255c17ab3eae2a0b5b4
Mirko Mariotti <mirko@mirkomariotti.it>
2022-09-07 13:08:33 +0200

    header and fonts options

--- cmd/brvgasdl/brvgasdl.go
+++ cmd/brvgasdl/brvgasdl.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
-	"github.com/BondMachineHQ/BondMachine/brvgasdl"
+	"github.com/BondMachineHQ/BondMachine/brvga/brvgasdl"
 )
 
 var verbose = flag.Bool("v", false, "Verbose")
@@ -12,18 +11,27 @@ var debug = flag.Bool("d", false, "Verbose")
 
 var constraint = flag.String("constraints", "", "Use a BM constraint file to set the VGA up")
 var sockFile = flag.String("sock", "/tmp/brvga.sock", "Socket file to use")
+var headerPath = flag.String("header", "", "Header file to use")
+var fontsPath = flag.String("fonts", "", "Fonts file to use")
 
 func init() {
 	flag.Parse()
 	if *constraint == "" {
 		panic("Must specify a constraint string")
 	}
+	if *headerPath == "" {
+		panic("Must specify a header file")
+	}
+	if *fontsPath == "" {
+		panic("Must specify a fonts file")
+	}
 }
 
 func main() {
-	if mem, err := brvgasdl.NewBrvgaSdl(*constraint); err != nil {
+	if mem, err := brvgasdl.NewBrvgaSdlUnixSock(*constraint, *sockFile, *headerPath, *fontsPath); err != nil {
 		panic(err)
 	} else {
-		fmt.Print(mem.Dump())
+		mem.Run()
+		defer mem.Close()
 	}
 }
