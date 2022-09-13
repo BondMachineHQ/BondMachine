6029f5e494a3d7acc8fc812dc93ac7f8bd169bf2
Mirko Mariotti <mirko@mirkomariotti.it>
2022-09-13 16:35:48 +0200

    neurons improved

--- neuralbond/neuralbond.go
+++ neuralbond/neuralbond.go
@@ -87,8 +87,10 @@ func (n *TrainedNet) WriteBasm() (string, error) {
 			result += fmt.Sprintf("%%meta ioatt input_%d cp:bm, type:input, index:%d\n", node.Pos, node.Pos)
 		} else if node.Type == "linear" {
 			result += fmt.Sprintf("%%meta cpdef node_%d_%d romcode:linear, inputs:%d, bias:0f%f\n", node.Layer, node.Pos, node.Inputs, node.Bias)
+		} else if node.Type == "summation" {
+			result += fmt.Sprintf("%%meta cpdef node_%d_%d romcode:summation, inputs:%d, bias:0f%f\n", node.Layer, node.Pos, node.Inputs, node.Bias)
 		} else if node.Type == "softmax" {
-			result += fmt.Sprintf("%%meta cpdef node_%d_%d romcode:softmax, inputs:%d, bias:0f%f\n", node.Layer, node.Pos, node.Inputs, node.Bias)
+			result += fmt.Sprintf("%%meta cpdef node_%d_%d romcode:softmax, inputs:%d, pos:%d\n", node.Layer, node.Pos, node.Inputs, node.Pos)
 		} else if node.Type == "output" {
 			result += fmt.Sprintf("%%meta cpdef node_%d_%d romcode:terminal\n", node.Layer, node.Pos)
 			result += fmt.Sprintf("%%meta iodef output_%d type:io\n", node.Pos)
