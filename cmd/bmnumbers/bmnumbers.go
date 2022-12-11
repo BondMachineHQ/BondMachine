564e02ca9a8b15172a2abbc183763c885d8e7916
Mirko Mariotti <mirko@mirkomariotti.it>
2022-12-11 15:45:44 +0100

    Minor

--- cmd/bmnumbers/bmnumbers.go
+++ cmd/bmnumbers/bmnumbers.go
@@ -1,5 +1,14 @@
 package main
 
+import "flag"
+
+var verbose = flag.Bool("v", false, "Verbose")
+var debug = flag.Bool("d", false, "Verbose")
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
 
 }
