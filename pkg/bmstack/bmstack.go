e366eef56424e44f9778e912a05d5f8a5d142293
Mirko Mariotti <mirko@mirkomariotti.it>
2023-01-10 17:15:53 +0100

    Stack and queues verilog generator

--- pkg/bmstack/bmstack.go
+++ pkg/bmstack/bmstack.go
@@ -2,32 +2,52 @@ package bmstack
 
 import (
 	"bytes"
+	"sort"
+	"strconv"
 	"text/template"
 )
 
-type stateMachine struct {
-	Nums     int
-	Bits     int
-	Buswidth string
-	Names    []string
-	Binary   []string
+// type stateMachine struct {
+// 	Nums     int
+// 	Bits     int
+// 	Buswidth string
+// 	Names    []string
+// 	Binary   []string
+// }
+
+type Push struct {
+	Agent string
+	Tick  uint64
+	Value string
+}
+
+type Pop struct {
+	Agent string
+	Tick  uint64
 }
 
 type BmStack struct {
 	ModuleName string
 	DataSize   int
+	Depth      int
 	Senders    []string
 	Receivers  []string
+	MemType    string
+	funcMap    template.FuncMap
 
-	Rsize       uint8
-	Buswidth    string
-	Inputs      []string
-	InputsBins  []string
-	Outputs     []string
-	OutputsBins []string
-	SendSM      stateMachine
-	funcMap     template.FuncMap
-	PackageName string
+	// TestBench data
+	Pops         []Pop
+	Pushes       []Push
+	TestSequence []string // Pushes and pops in order
+
+	// Rsize       uint8
+	// Buswidth    string
+	// Inputs      []string
+	// InputsBins  []string
+	// Outputs     []string
+	// OutputsBins []string
+	// SendSM      stateMachine
+	// PackageName string
 }
 
 func CreateBasicStack() *BmStack {
@@ -41,12 +61,15 @@ func CreateBasicStack() *BmStack {
 		"dec": func(i int) int {
 			return i - 1
 		},
-		"next": func(i int) int {
-			if i < result.SendSM.Nums-1 {
+		"next": func(i int, max int) int {
+			if i < max-1 {
 				return i + 1
 			} else {
 				return 0
 			}
+		},
+		"bits": func(i int) int {
+			return NeededBits(i)
 		},
 	}
 	result.funcMap = funcMap
@@ -73,4 +96,126 @@ func (s *BmStack) WriteHDL() (string, error) {
 
 	return f.String(), nil
 
+}
+
+func (s *BmStack) WriteTestBench() (string, error) {
+
+	var f bytes.Buffer
+
+	// Sort the pushes and pops by tick
+	sort.Slice(s.Pushes, func(i, j int) bool {
+		return s.Pushes[i].Tick < s.Pushes[j].Tick
+	})
+	sort.Slice(s.Pops, func(i, j int) bool {
+		return s.Pops[i].Tick < s.Pops[j].Tick
+	})
+
+	s.TestSequence = make([]string, 0)
+
+	var subSeq []string
+	var absTick uint64
+	var oldAbsTick uint64
+
+	for i, j := 0, 0; i < len(s.Pushes) || j < len(s.Pops); {
+		if i == len(s.Pushes) {
+			absTick = s.Pops[j].Tick
+			if absTick != oldAbsTick {
+				relTick := absTick - oldAbsTick
+				s.TestSequence = append(s.TestSequence, subSeq...)
+				s.TestSequence = append(s.TestSequence, "")
+				s.TestSequence = append(s.TestSequence, "#"+strconv.Itoa(int(relTick))+";")
+				subSeq = make([]string, 0)
+				oldAbsTick += relTick
+			}
+			agent := s.Pops[j].Agent
+			tick := strconv.Itoa(int(absTick))
+			subSeq = append(subSeq, "// Pop agent "+agent+" at tick "+tick)
+			subSeq = append(subSeq, agent+"Impulse=1;")
+			subSeq = append(subSeq, "#5;")
+			subSeq = append(subSeq, agent+"Impulse=0;")
+			j++
+		} else if j == len(s.Pops) {
+			absTick = s.Pushes[i].Tick
+			if absTick != oldAbsTick {
+				relTick := absTick - oldAbsTick
+				s.TestSequence = append(s.TestSequence, subSeq...)
+				s.TestSequence = append(s.TestSequence, "")
+				s.TestSequence = append(s.TestSequence, "#"+strconv.Itoa(int(relTick))+";")
+				subSeq = make([]string, 0)
+				oldAbsTick += relTick
+			}
+			agent := s.Pushes[i].Agent
+			value := s.Pushes[i].Value
+			tick := strconv.Itoa(int(absTick))
+			subSeq = append(subSeq, "// Push agent "+agent+" at tick "+tick+" with value "+value)
+			subSeq = append(subSeq, agent+"Data="+value+";")
+			subSeq = append(subSeq, agent+"Impulse=1;")
+			subSeq = append(subSeq, "#5;")
+			subSeq = append(subSeq, agent+"Impulse=0;")
+			i++
+		} else {
+			if s.Pushes[i].Tick <= s.Pops[j].Tick {
+				absTick = s.Pushes[i].Tick
+				if absTick != oldAbsTick {
+					relTick := absTick - oldAbsTick
+					s.TestSequence = append(s.TestSequence, subSeq...)
+					s.TestSequence = append(s.TestSequence, "")
+					s.TestSequence = append(s.TestSequence, "#"+strconv.Itoa(int(relTick))+";")
+					subSeq = make([]string, 0)
+					oldAbsTick += relTick
+				}
+				agent := s.Pushes[i].Agent
+				value := s.Pushes[i].Value
+				tick := strconv.Itoa(int(absTick))
+				subSeq = append(subSeq, "// Push agent "+agent+" at tick "+tick+" with value "+value)
+				subSeq = append(subSeq, agent+"Data="+value+";")
+				subSeq = append(subSeq, agent+"Impulse=1;")
+				subSeq = append(subSeq, "#5;")
+				subSeq = append(subSeq, agent+"Impulse=0;")
+				i++
+			} else {
+				absTick = s.Pops[j].Tick
+				if absTick != oldAbsTick {
+					relTick := absTick - oldAbsTick
+					s.TestSequence = append(s.TestSequence, subSeq...)
+					s.TestSequence = append(s.TestSequence, "")
+					s.TestSequence = append(s.TestSequence, "#"+strconv.Itoa(int(relTick))+";")
+					subSeq = make([]string, 0)
+					oldAbsTick += relTick
+				}
+				agent := s.Pops[j].Agent
+				tick := strconv.Itoa(int(absTick))
+				subSeq = append(subSeq, "// Pop agent "+agent+" at tick "+tick)
+				subSeq = append(subSeq, agent+"Impulse=1;")
+				subSeq = append(subSeq, "#5;")
+				subSeq = append(subSeq, agent+"Impulse=0;")
+				j++
+			}
+		}
+	}
+	s.TestSequence = append(s.TestSequence, subSeq...)
+
+	t, err := template.New("testbench").Funcs(s.funcMap).Parse(testbench)
+	if err != nil {
+		return "", err
+	}
+
+	err = t.Execute(&f, *s)
+	if err != nil {
+		return "", err
+	}
+
+	return f.String(), nil
+
+}
+
+func NeededBits(num int) int {
+	if num > 0 {
+		for bits := 1; true; bits++ {
+			if 1<<uint8(bits) >= num {
+				return bits
+			}
+		}
+	}
+	return 0
 }
--- pkg/bmstack/bmstack_test.go
+++ pkg/bmstack/bmstack_test.go
@@ -1,15 +1,45 @@
 package bmstack
 
 import (
-	"fmt"
+	"os"
 	"testing"
 )
 
 func TestStack(t *testing.T) {
 	s := CreateBasicStack()
 	s.DataSize = 32
+	s.Depth = 8
+	s.MemType = "FIFO"
 	s.Senders = []string{"sender1", "sender2"}
-	s.Receivers = []string{"receiver1", "receiver2"}
+	s.Receivers = []string{"receiver1"}
 
-	fmt.Println(s.WriteHDL())
+	s.Pushes = []Push{
+		Push{"sender1", 200, "32'd1"},
+	}
+
+	s.Pops = []Pop{
+		Pop{"receiver1", 60},
+	}
+
+	f, err := os.Create("stack.v")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	if r, err := s.WriteHDL(); err != nil {
+		t.Error(err)
+	} else {
+		f.WriteString(r)
+	}
+
+	tb, err := os.Create("stack_tb.v")
+	if err != nil {
+		t.Error(err)
+	}
+	defer tb.Close()
+	if r, err := s.WriteTestBench(); err != nil {
+		t.Error(err)
+	} else {
+		tb.WriteString(r)
+	}
 }
