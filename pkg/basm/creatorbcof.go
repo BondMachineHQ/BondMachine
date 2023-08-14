6247da63d55a917df7c5db2b8d33eeb56cfaae12
Mirko Mariotti <mirko@mirkomariotti.it>
2023-08-14 23:31:08 +0200

    BCOF creation skel

--- pkg/basm/creatorbcof.go
+++ pkg/basm/creatorbcof.go
@@ -1,7 +1,35 @@
 package basm
 
+import "fmt"
+
 // Assembler2BCOF transform an assembled instance into a BCOF file
 func (bi *BasmInstance) Assembler2BCOF() error {
+
+	for _, cp := range bi.cps {
+		if bi.debug {
+			fmt.Println("\t\t" + green("CP: ") + yellow(cp.GetValue()))
+		}
+
+		ramCode := cp.GetMeta("ramcode")
+		if bi.debug {
+			if ramCode != "" {
+				fmt.Println("\t\t - " + green("ram code: ") + yellow(ramCode))
+			} else {
+				fmt.Println("\t\t - " + green("ram code: ") + yellow("not specified"))
+			}
+		}
+
+		ramData := cp.GetMeta("ramdata")
+		if bi.debug {
+			if ramData != "" {
+				fmt.Println("\t\t - " + green("ram data: ") + yellow(ramData))
+			} else {
+				fmt.Println("\t\t - " + green("ram data: ") + yellow("not specified"))
+			}
+		}
+		// if the name of the CP (cp.GetValue()) is in the form of "cpN" (where N is a number) then we have to compile against the N-th CP of the result BM
+		// otherwise we have to compile against the CP with the same name that can be found into the bminfo file
+	}
 	//TODO
 	return nil
 }
