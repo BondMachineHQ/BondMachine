4a2ab2239721ecce4d09608805e451e914b72602
Mirko Mariotti <mirko@mirkomariotti.it>
2023-08-25 14:10:13 +0200

    choices

--- pkg/basm/matcherresolver.go
+++ pkg/basm/matcherresolver.go
@@ -3,11 +3,19 @@ package basm
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/BondMachineHQ/BondMachine/pkg/bmline"
 	"github.com/BondMachineHQ/BondMachine/pkg/bmreqs"
 	"github.com/BondMachineHQ/BondMachine/pkg/procbuilder"
 )
+
+type choice struct {
+	choiceName string
+	choiceSel  string
+}
+type choices []choice
 
 func matcherResolver(bi *BasmInstance) error {
 	//TODO finish this
@@ -16,6 +24,105 @@ func matcherResolver(bi *BasmInstance) error {
 		fmt.Println(green("\tProcessing sections:"))
 	}
 
+	// sectionsIncoming := make(map[string]*BasmSection)
+	// sectionsOutgoing := make(map[string]struct{})
+
+	// Loop over the sections to find eventual alternatives
+	for sectName, section := range bi.sections {
+		if section.sectionType == sectRomText || section.sectionType == sectRamText {
+			if bi.debug {
+				fmt.Println(green("\t\tSection: ") + sectName)
+			}
+
+			// Setup the map of alternatives sections based on the choices
+			sectAlts := make(map[string]choices)
+
+			body := section.sectionBody
+
+			for _, line := range body.Lines {
+
+				if bi.debug {
+					fmt.Println(green("\t\t\tLine: ") + line.String())
+				}
+
+				matched := false
+				matching := make([]string, 0)
+
+				for j, matcher := range bi.matchers {
+					if bmline.MatchMatcher(matcher, line) {
+						if bi.debug {
+							fmt.Println(yellow("\t\t\t\tMatching " + matcher.String()))
+						}
+						matched = true
+						matching = append(matching, strconv.Itoa(j))
+					}
+				}
+				// fmt.Println(matching)
+				if !matched {
+					return errors.New("no operator match")
+				} else if len(matching) > 1 {
+					found := false
+					choiceName := strings.Join(matching, "_")
+				sLoop:
+					for _, cs := range sectAlts {
+						for _, c := range cs {
+							if c.choiceName == choiceName {
+								found = true
+								break sLoop
+							}
+						}
+					}
+					// There is a choice to be made, creating the alternatives names and the choices for the sections
+					if !found {
+						sectRem := make(map[string]struct{})
+
+						if len(sectAlts) == 0 {
+							for _, c := range matching {
+								idx := 0
+							idxLoop1:
+								// Identify the next available index
+								for {
+									if _, ok := bi.sections[sectName+"_"+strconv.Itoa(idx)]; !ok {
+										if _, ok := sectAlts[sectName+"_"+strconv.Itoa(idx)]; !ok {
+											break idxLoop1
+										}
+									}
+									idx++
+								}
+								// Create the new section with the new name and the choice
+								sectAlts[sectName+"_"+strconv.Itoa(idx)] = choices{choice{choiceName: choiceName, choiceSel: c}}
+							}
+						} else {
+							for s, cs := range sectAlts {
+								for _, c := range matching {
+									idx := 0
+								idxLoop2:
+									// Identify the next available index
+									for {
+										if _, ok := bi.sections[sectName+"_"+strconv.Itoa(idx)]; !ok {
+											if _, ok := sectAlts[sectName+"_"+strconv.Itoa(idx)]; !ok {
+												break idxLoop2
+											}
+										}
+										idx++
+									}
+									// Create the new section with the new name and the choice
+									sectAlts[sectName+"_"+strconv.Itoa(idx)] = append(cs, choice{choiceName: choiceName, choiceSel: c})
+								}
+								sectRem[s] = struct{}{}
+							}
+
+							// Remove the old sections
+							for s := range sectRem {
+								delete(sectAlts, s)
+							}
+						}
+					}
+				}
+			}
+			// TODO Finire da qui
+		}
+	}
 	// Loop over the sections
 	for sectName, section := range bi.sections {
 		if section.sectionType == sectRomText || section.sectionType == sectRamText {
@@ -38,7 +145,7 @@ func matcherResolver(bi *BasmInstance) error {
 				}
 
 				matched := false
-				var maching procbuilder.Opcode
+				var matching procbuilder.Opcode
 
 				for j, matcher := range bi.matchers {
 					if bmline.MatchMatcher(matcher, line) {
@@ -46,10 +153,10 @@ func matcherResolver(bi *BasmInstance) error {
 							fmt.Println(yellow("\t\t\t\tMaching " + matcher.String()))
 						}
 						if matched {
-							return errors.New("Ambiguous, more than one operator match")
+							return errors.New("ambiguous, more than one operator match")
 						}
 						matched = true
-						maching = bi.matchersOps[j]
+						matching = bi.matchersOps[j]
 					}
 				}
 
@@ -57,17 +164,17 @@ func matcherResolver(bi *BasmInstance) error {
 					return errors.New("no operator match")
 				}
 
-				opname := maching.Op_get_name()
+				opName := matching.Op_get_name()
 
 				if section.sectionType == sectRomText {
-					bi.rg.Requirement(bmreqs.ReqRequest{Node: "/code:romtexts/sections:" + sectName, T: bmreqs.ObjectSet, Name: "opcodes", Value: opname, Op: bmreqs.OpAdd})
+					bi.rg.Requirement(bmreqs.ReqRequest{Node: "/code:romtexts/sections:" + sectName, T: bmreqs.ObjectSet, Name: "opcodes", Value: opName, Op: bmreqs.OpAdd})
 				} else {
-					bi.rg.Requirement(bmreqs.ReqRequest{Node: "/code:ramtexts/sections:" + sectName, T: bmreqs.ObjectSet, Name: "opcodes", Value: opname, Op: bmreqs.OpAdd})
+					bi.rg.Requirement(bmreqs.ReqRequest{Node: "/code:ramtexts/sections:" + sectName, T: bmreqs.ObjectSet, Name: "opcodes", Value: opName, Op: bmreqs.OpAdd})
 				}
 
 				// Normalize instruction
 				if section.sectionType == sectRomText {
-					if normalized, err := maching.HLAssemblerNormalize(nil, bi.rg, "/code:romtexts/sections:"+sectName, line); err != nil {
+					if normalized, err := matching.HLAssemblerNormalize(nil, bi.rg, "/code:romtexts/sections:"+sectName, line); err != nil {
 						return err
 					} else {
 						if bi.debug {
@@ -76,7 +183,7 @@ func matcherResolver(bi *BasmInstance) error {
 						body.Lines[i] = normalized
 					}
 				} else {
-					if normalized, err := maching.HLAssemblerNormalize(nil, bi.rg, "/code:ramtexts/sections:"+sectName, line); err != nil {
+					if normalized, err := matching.HLAssemblerNormalize(nil, bi.rg, "/code:ramtexts/sections:"+sectName, line); err != nil {
 						return err
 					} else {
 						if bi.debug {
