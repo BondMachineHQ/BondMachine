de085d1b957cc5c663d1f738c8131bdd06e0d1ef
Mirko Mariotti <mirko@mirkomariotti.it>
2024-09-20 23:51:32 +0200

    Docs

--- pkg/bmstack/bmstack_test.go
+++ pkg/bmstack/bmstack_test.go
@@ -10,15 +10,25 @@ func TestStack(t *testing.T) {
 	s.DataSize = 32
 	s.Depth = 8
 	s.MemType = "FIFO"
-	s.Senders = []string{"sender1", "sender2"}
-	s.Receivers = []string{"receiver1"}
+	s.Senders = []string{"sender1", "sender2", "sender3"}
+	s.Receivers = []string{"receiver1", "receiver2"}
 
 	s.Pushes = []Push{
-		Push{"sender1", 200, "32'd1"},
+		Push{"sender1", 100, "32'd1"},
+		Push{"sender2", 150, "32'd2"},
+		Push{"sender3", 200, "32'd3"},
+		Push{"sender1", 250, "32'd4"},
+		Push{"sender2", 300, "32'd5"},
+		Push{"sender3", 350, "32'd6"},
 	}
 
 	s.Pops = []Pop{
-		Pop{"receiver1", 60},
+		Pop{"receiver1", 110},
+		Pop{"receiver2", 160},
+		Pop{"receiver1", 210},
+		Pop{"receiver2", 260},
+		Pop{"receiver1", 310},
+		Pop{"receiver2", 360},
 	}
 
 	// s.SaveJSON("stack.json")
