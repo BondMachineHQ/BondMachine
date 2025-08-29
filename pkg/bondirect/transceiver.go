68c95e09ee7156fa1cc8be6af924d10803884c0f
Mirko Mariotti <mirko@mirkomariotti.it>
2025-08-29 12:19:42 +0200

    :sparkles: (bondirect) tranceiver generator skel

--- pkg/bondirect/transceiver.go
+++ pkg/bondirect/transceiver.go
@@ -1,6 +1,29 @@
 package bondirect
 
+import (
+	"bytes"
+	"text/template"
+)
+
 func (be *BondirectElement) GenerateTransceiver(prefix, nodeName, edgeName, direction string) (string, error) {
 	// Implementation for generating a Transceiver
-	return "", nil
+
+	trn := bondRx
+	if direction == "out" {
+		trn = bondTx
+	}
+
+	var f bytes.Buffer
+
+	t, err := template.New("transceiver").Funcs(funcMap).Parse(trn)
+	if err != nil {
+		return "", err
+	}
+
+	err = t.Execute(&f, be.Tdata)
+	if err != nil {
+		return "", err
+	}
+
+	return f.String(), nil
 }
